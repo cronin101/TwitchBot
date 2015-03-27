517b3225587c63598c55a7401830b9176f9d7b9d
Aaron Cronin <cronin@include.cat>
2015-03-27 21:21:45 +0100

    Break up summary method

--- gambling/bet_handler.rb
+++ gambling/bet_handler.rb
@@ -11,18 +11,6 @@ module BetHandler
     self.current_round = new_round.number
   end
 
-  def this_round
-    Bet.where(round: self.current_round)
-  end
-
-  def bets_for
-    this_round.where(is_on_victory: true)
-  end
-
-  def bets_against
-    this_round.where(is_on_victory: false)
-  end
-
   def payout(victory)
     bets = victory ? bets_for : bets_against
     bets.map do |bet|
@@ -33,26 +21,16 @@ module BetHandler
 
       "#{user.name}: You now have #{user.coins} jaggCoins! (+ #{bet.amount})"
     end
-
   end
 
   def summarise_round
-    round = Bet.where(round: self.current_round)
-
-    if round.count == 0
-      return ["No bets were placed during the last round!"]
-    elsif bets_for.count == bets_against.count
-      status = "It's looking pretty uncertain with an even split of #{round.count} people. Half (#{bets_for.count}) expect a win; the same number (#{bets_against.count}) predict a loss!"
-    elsif bets_for.count > bets_against.count
-      status = "Most people (#{bets_for.count} / #{round.count}) believe that Jaggerous has what it takes to win the next game."
-    else
-      status = "There is a distinct lack of confidence in Jaggerous for the next game, with (#{bets_against.count} / #{round.count}) people expecting a loss."
-    end
-
-    total   = ->(ary) { ary.map(&:amount).inject(:+) || 0 }
-    average = ->(ary) { ary.map(&:amount).map(&:to_f).inject(:+) / ary.count.to_f rescue 0 }
-
-    return [status] << "#{total.(bets_for)} jaggCoins placed on Victory (average: #{average.(bets_for)}), #{total.(bets_against)} placed on Defeat (average: #{average.(bets_against)})!"
+    case betting_trend
+    when :no_bets then return ["No bets were placed during the last round!"]
+    when :tie then split_summary
+    when :victory then optimistic_summary
+    when :loss then pessimistic_summary
+    end <<
+      "#{total_of bets_for} jaggCoins placed on Victory (average: #{average_of bets_for}), #{total_of bets_against} placed on Defeat (average: #{average_of bets_against})!"
   end
 
   Response = Struct.new(:success, :messages)
@@ -93,5 +71,53 @@ module BetHandler
         r.messages = ["Sorry, you don't have the funds to make that wager! You only have #{user.coins} jaggCoins in your piggy-bank."]
       end
     end
+  end
+
+  private
+
+  def total_of bets
+    bets.map(&:amount).inject(:+) || 0
+  end
+
+  def average_of bets
+    amounts = bets.map(&:amount)
+    amounts.inject(:+).to_f / amounts.count rescue 0
+  end
+
+  def this_round
+    Bet.where(round: self.current_round)
+  end
+
+  def bets_for
+    this_round.where(is_on_victory: true)
+  end
+
+  def bets_against
+    this_round.where(is_on_victory: false)
+  end
+
+  def betting_trend
+    if this_round.count == 0
+      :no_bets
+    elsif bets_for.count == bets_against.count
+      :tie
+    elsif bets_for.count > bets_against.count
+      :victory
+    else
+      :loss
+    end
+  end
+
+  def split_summary
+    ["It's looking pretty uncertain with an even split of #{this_round.count} people.",
+     "Half (#{bets_for.count}) expect a win; the same number (#{bets_against.count}) predict a loss!"]
+  end
+
+  def optimistic_summary
+    ["Most people (#{bets_for.count} / #{this_round.count}) believe that Jaggerous has what it takes to win the next game."]
+  end
+
+  def pessimistic_summary
+    ["There is a distinct lack of confidence in Jaggerous for the next game, with (#{bets_against.count} / #{this_round.count}) people expecting a loss."]
   end
 end
