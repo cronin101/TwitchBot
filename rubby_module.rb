2df28c1bfcfe8d2b53be5aaa6d3a1800350ae5be
Aaron Cronin <cronin@include.cat>
2015-04-08 19:32:16 +0200

    Extract method in Rubby

--- rubby_module.rb
+++ rubby_module.rb
@@ -11,11 +11,15 @@ module Rubby
     method_name.tap do |name|
       body = instance_method name
       define_method name do |*args, &block|
-        if !Rubby::THROTTLING[:active] || last_call.nil? || (last_call + duration <=  Rubby.current_time)
+        if Rubby.can_execute last_call, duration
           last_call = Rubby.current_time
           body.bind(self).call(*args, &block)
         end
       end
     end
+  end
+
+  def self.can_execute(last_call, interval)
+    !Rubby::THROTTLING[:active] || last_call.nil? || (last_call + interval <=  Rubby.current_time)
   end
 end
